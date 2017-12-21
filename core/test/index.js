7ce550a0a74d2d14f9797fcbc7674ad356c1b23c
Marcus <marcusposey@gmail.com>
2017-12-20 22:05:58 -0600

    Fix async bug

--- core/test/index.js
+++ core/test/index.js
@@ -4,8 +4,10 @@ const { db } = require('../service/database');
 
 /** Removes all rows from each table in the database */
 module.exports.clearDB = async () => {
-    await ['note_tags', 'note_watchers', 'notes', 'tags', 'users']
-        .forEach(async (table) => await db(table).del());
+    const tables = ['note_tags', 'note_watchers', 'notes', 'tags', 'users'];
+    for (let table of tables) {
+        await db(table).del();
+    }
 };
 
 // The test process will hang if the database connection is not closed.
