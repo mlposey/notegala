9362657b4b0c91ddbdce8bde2901a78288169f92
Marcus <marcusposey@gmail.com>
2017-12-20 21:00:42 -0600

    Make export style match other files

--- core/gql-spec/resolver.js
+++ core/gql-spec/resolver.js
@@ -3,7 +3,7 @@ const { GraphQLError } = require('graphql');
 const Account = require('../model/account');
 
 // Root resolver for all GraphQL queries
-exports.root = {
+module.exports.root = {
     account: (root, {email, name}, context) => {
         // TODO: account query
         return new Account(0, '', '', 'placeholder@example.com', 'Placeholder');
