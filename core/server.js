b8ac69f2497c833fed678bd5477578509c0926a3
Marcus <marcusposey@gmail.com>
2017-12-20 18:24:05 -0600

    Hook auth middleware to express

--- core/server.js
+++ core/server.js
@@ -1,11 +1,27 @@
 'use strict';
 const express = require('express');
+const AuthMiddleware = require('./service/auth-middleware');
 
 var app = express();
 app.use('/status', (req, res, next) => {
     // TODO: Check the health of the database connection.
     res.status(200).end();
 });
+
+// Conditionally verify requests based on test/production environment.
+if (process.env.NODE_ENV != 'test') {
+    let auth = new AuthMiddleware(process.env.CLIENT_ID);
+    app.use(auth.verifyGId.bind(auth));
+} else {
+    let auth = new AuthMiddleware('test-client-id');    
+    app.use((req, res, next) => {
+        auth.storeClaims(req, {
+            email: 'test',
+            name: 'Jane Doe'
+        })
+        next();
+    });
+}
 
 app.listen(8080);
 console.log('server started');
