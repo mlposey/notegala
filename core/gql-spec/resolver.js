7166345121d194ce43e497dbc539bcfd0475ed3e
Marcus <marcusposey@gmail.com>
2017-12-20 22:20:35 -0600

    Finish the 'account' query

--- core/gql-spec/resolver.js
+++ core/gql-spec/resolver.js
@@ -5,7 +5,15 @@ const Account = require('../model/account');
 // Root resolver for all GraphQL queries
 module.exports.root = {
     account: (root, {email, name}, context) => {
-        // TODO: account query
-        return new Account(0, '', '', 'placeholder@example.com', 'Placeholder');
+        return Account.fromEmail(email)
+            .then(acct => {
+                return acct;
+            })
+            .catch(e => {
+                return Account.construct(email, name);
+            })
+            .catch(e => {
+                return new GraphQLError(e.message);
+            })
     }
 };
