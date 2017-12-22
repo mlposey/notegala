d53ad08b54d986a6a363e58d12d7e6ed60f7e655
Marcus <marcusposey@gmail.com>
2017-12-22 09:22:21 -0600

    Implement controller for the createNote mutation

--- core/gql-spec/resolver.js
+++ core/gql-spec/resolver.js
@@ -1,8 +1,9 @@
 'use strict';
 const { GraphQLError } = require('graphql');
 const Account = require('../model/account');
+const Note = require('../model/note/note');
 
-// Root resolver for all GraphQL queries
+// Root resolver for all GraphQL queries and mutations
 module.exports.root = {
     account: (root, {email, name}, context) => {
         return Account.fromEmail(email)
@@ -14,6 +15,11 @@ module.exports.root = {
             })
             .catch(e => {
                 return new GraphQLError(e.message);
-            })
+            });
+    },
+    createNote: async (root, {email}, context) => {
+        const newNoteInput = context.variableValues.input;
+        try { return await Note.construct(email, newNoteInput); }
+        catch (e) { return new GraphQLError(e.message); }
     }
 };
