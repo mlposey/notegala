8424161e31d913299a7bb813b65dfd1a6898a9c8
Marcus <marcusposey@gmail.com>
2017-12-23 01:39:09 -0600

    Hook the note fab to NoteActivity

--- android/app/src/main/java/com/marcusposey/notegala/NotesFragment.java
+++ android/app/src/main/java/com/marcusposey/notegala/NotesFragment.java
@@ -1,10 +1,14 @@
 package com.marcusposey.notegala;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 /** Handles a homogeneous collection of notes */
 public class NotesFragment extends Fragment {
@@ -15,8 +19,25 @@ public class NotesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes, container, false);
+        View root = inflater.inflate(R.layout.fragment_notes, container, false);
+
+        configureFloatingActionButtons(root);
+        return root;
     }
 
+    private void configureFloatingActionButtons(View rootView) {
+        FloatingActionMenu menu = rootView.findViewById(R.id.fab_menu);
+
+        FloatingActionButton newNoteButton = rootView.findViewById(R.id.fab_note);
+        newNoteButton.setOnClickListener(view -> {
+            menu.close(true);
+            Intent intent = new Intent(getContext(), NoteActivity.class);
+            startActivity(intent);
+        });
+
+        FloatingActionButton newNotebookButton = rootView.findViewById(R.id.fab_notebook);
+        newNotebookButton.setOnClickListener(view -> {
+            menu.close(true);
+        });
+    }
 }
