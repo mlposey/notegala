5e699221b921075bd2e6f2017845c0490d1c5f5a
Marcus <marcusposey@gmail.com>
2017-12-23 18:12:48 -0600

    Create adapter for note cards

---
+++ android/app/src/main/java/com/marcusposey/notegala/note/MyNoteAdapter.java
@@ -0,0 +1,41 @@
+package com.marcusposey.notegala.note;
+
+import android.content.Context;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.marcusposey.notegala.R;
+import com.marcusposey.notegala.net.gen.MyNotesQuery;
+
+/** Maps owned notes to cards that display summaries of content */
+public class MyNoteAdapter extends ArrayAdapter<MyNotesQuery.Note> {
+    private final Context mContext;
+    private final FragmentManager mFragmentManager;
+    private final MyNotesQuery.Note[] mNotes;
+
+    public MyNoteAdapter(Context context, FragmentManager manager, MyNotesQuery.Note[] notes) {
+        super(context, -1, notes);
+        mContext = context;
+        mFragmentManager = manager;
+        mNotes = notes;
+    }
+
+    @Override
+    public View getView(int pos, View convertView, ViewGroup parent) {
+        // Show new notes first.
+        pos = mNotes.length - 1 - pos;
+
+        LayoutInflater inflater =
+                (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.content_my_note, parent, false);
+
+        ((TextView) row.findViewById(R.id.my_note_title)).setText(mNotes[pos].title());
+        ((TextView) row.findViewById(R.id.my_note_body)).setText(mNotes[pos].body());
+
+        return row;
+    }
+}
