4bb32bc22b509b8380b24ee051efc27afa76e343
Marcus <marcusposey@gmail.com>
2017-12-23 18:34:32 -0600

    Display owned notes when on the 'My Notes' tab

--- android/app/src/main/java/com/marcusposey/notegala/note/MyNotesFragment.java
+++ android/app/src/main/java/com/marcusposey/notegala/note/MyNotesFragment.java
@@ -3,6 +3,7 @@ package com.marcusposey.notegala.note;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,15 @@ import android.widget.Toast;
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 import com.marcusposey.notegala.R;
+import com.marcusposey.notegala.net.QueryService;
+import com.marcusposey.notegala.net.gen.MyNotesQuery;
+
+import java.util.List;
 
 /** Handles notes that the user owns */
 public class MyNotesFragment extends ListFragment {
+    private static final String LOG_TAG = MyNotesFragment.class.getSimpleName();
+
     public MyNotesFragment() {
         // Required empty public constructor
     }
@@ -24,6 +31,7 @@ public class MyNotesFragment extends ListFragment {
         View root = inflater.inflate(R.layout.fragment_my_notes, container, false);
 
         configureFloatingActionButtons(root);
+        QueryService.awaitInstance(service -> service.getMyNotes(this::onNotesNetworkResponse));
         return root;
     }
 
@@ -41,6 +49,22 @@ public class MyNotesFragment extends ListFragment {
         newNotebookButton.setOnClickListener(view -> {
             Toast.makeText(getContext(), "not yet implemented", Toast.LENGTH_SHORT).show();
             menu.close(true);
+        });
+    }
+
+    /** Handles the network response for a notes request */
+    private void onNotesNetworkResponse(Exception e, List<MyNotesQuery.Note> notes) {
+        getActivity().runOnUiThread(() -> {
+            Log.i(LOG_TAG, "got notes network response");
+
+            if (e != null) {
+                Log.e(LOG_TAG, e.getMessage());
+                Toast.makeText(getActivity(), "network error", Toast.LENGTH_LONG).show();
+            } else {
+                Log.i(LOG_TAG, String.format("fetched %d notes", notes.size()));
+                setListAdapter(new MyNoteAdapter(getActivity(), getFragmentManager(),
+                        notes.toArray(new MyNotesQuery.Note[0])));
+            }
         });
     }
 }
