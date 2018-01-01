b95ddc316606e04848c0675257197546cefe64d3
Marcus <marcusposey@gmail.com>
2017-12-31 20:33:57 -0600

    Remove unused import

--- android/app/src/main/java/com/marcusposey/notegala/note/NoteActivity.java
+++ android/app/src/main/java/com/marcusposey/notegala/note/NoteActivity.java
@@ -1,6 +1,5 @@
 package com.marcusposey.notegala.note;
 
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
