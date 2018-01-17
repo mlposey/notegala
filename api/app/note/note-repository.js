d57fb9f73d0c976ebb5f3e0f950c218522005e5c
Marcus <marcusposey@gmail.com>
2018-01-17 12:21:28 -0600

    Create repository to manage the collection of notes

---
+++ api/app/note/note-repository.js
@@ -0,0 +1,81 @@
+'use strict';
+const { db } = require('../data/database');
+const { Repository, NotFoundError } = require('../data/repository');
+const Note = require('./note');
+
+/** Manages the global collection of Notes */
+module.exports = class NoteRepository extends Repository {
+    /**
+     * Adds a new note to the repository
+     * @param {Note} note Will be updated to match the version as it
+     *                    exists in the repository
+     * @throws {Error} If note.owner is not a valid account id
+     */
+    async add(note) {
+        const rows = await db('notes')
+            .insert({
+                owner_id: note.owner,
+                title: note.title,
+                body: note.body,
+                is_public: note.isPublic
+            })
+            .returning(['id', 'created_at', 'last_modified'])
+            .catch(err => { throw new Error('invalid owner id'); });
+        
+        note.id = rows[0].id;
+        note.createdAt = rows[0].created_at;
+        note.lastModified = rows[0].last_modified;
+    }
+
+    /**
+     * Removes the note from the repository, detaching any tags
+     * @param {Note} note 
+     * @throws {NotFoundError} If the note is not in the repository
+     */
+    async remove(note) {
+        await db('note_tags')
+            .where({note_id: note.id})
+            .del();
+        const rows = await db('notes')
+            .where({id: note.id})
+            .del()
+            .returning('id');
+        
+        if (rows.length === 0) throw new NotFoundError();
+    }
+
+    /**
+     * Replaces the existing note in the repository
+     * @param {Note} note An updated note
+     *                    Note: The id must remain the same
+     * @throws {NotFoundError} If the note is not in the repository
+     */
+    async replace(note) {
+        const rows = await db('notes')
+            .update({
+                is_public: note.isPublic,
+                title: note.title,
+                body: note.body
+            })
+            .where({id: note.id})
+            .returning('id');
+
+        if (rows.length === 0) throw new NotFoundError();
+    }
+
+    /**
+     * Finds all notes in the repository that match the specification
+     * @param {Specification} spec
+     * @param {number} limit The maximum number of notes to retrieve
+     * @return {Promise.<Array.<Note>>}
+     */
+    async find(spec, limit = this.DEFAULT_LIMIT) {
+        let rows = await spec.toQuery().limit(limit);
+        return rows.map(row => new Note(row.owner_id, row.title, row.body, {
+            id: row.id,
+            createdAt: row.created_at,
+            lastModified: row.last_modified,
+            isPublic: row.is_public
+        }));
+    }
+};
---
+++ api/test/note-repository.test.js
@@ -0,0 +1,111 @@
+'use strict';
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+
+const { clearDB } = require('./index');
+const { db } = require('../app/data/database');
+const Account = require('../app/account/account');
+const AccountRepository = require('../app/account/account-repository');
+const Note = require('../app/note/note');
+const NoteRepository = require('../app/note/note-repository');
+const NoteIdSpec = require('../app/note/id-spec');
+
+// Models potential data supplied by a createNote request
+const payload = Object.freeze({
+    email: 'test@example.com',
+    name: 'Trusty Tester',
+    input: {
+        title: 'Test Title',
+        body: 'This is a test.',
+        tags: ['test', 'example', 'mock']
+    }
+});
+
+const accountRepo = new AccountRepository();
+const noteRepo = new NoteRepository();
+
+describe('NoteRepository', () => {
+    let acct;
+    let note;
+    describe('#add(note)', () => {
+        beforeEach(async () => {
+            await clearDB();
+            acct = new Account(payload.email, payload.name);
+            await accountRepo.add(acct);
+            note = new Note(acct.id, payload.input.title, payload.input.body);
+        });
+
+        it('should add the note if the note.owner is valid', async () => {
+            await noteRepo.add(note);
+            const matches = await noteRepo.find(new NoteIdSpec(note.id));
+            matches.length.should.eql(1);
+        });
+
+        it('should throw exception if note.owner is invalid', async () => {
+            note.owner = -1;
+
+            let wasThrown = false;
+            try { await noteRepo.add(note); }
+            catch (err) { wasThrown = true; }
+            
+            wasThrown.should.eql(true);
+        });
+    });
+
+    describe('#remove(note)', () => {
+        beforeEach(async () => {
+            await clearDB();
+            acct = new Account(payload.email, payload.name);
+            await accountRepo.add(acct);
+            note = new Note(acct.id, payload.input.title, payload.input.body);
+            await noteRepo.add(note);            
+        });
+
+        it('should remove the note if it is recognized', async () => {
+            await noteRepo.remove(note);
+            const matches = await noteRepo.find(new NoteIdSpec(note.id));
+            matches.length.should.eql(0);
+        });
+
+        it('should throw NotFoundError if unrecognized', async () => {
+            note.id++;
+
+            let wasThrown = false;
+            try { await noteRepo.remove(note); }
+            catch (err) { wasThrown = true; }
+
+            wasThrown.should.eql(true);
+        });
+    });
+
+    describe('#replace(note)', () => {
+        beforeEach(async () => {
+            await clearDB();
+            acct = new Account(payload.email, payload.name);
+            await accountRepo.add(acct);
+            note = new Note(acct.id, payload.input.title, payload.input.body);
+            await noteRepo.add(note);            
+        });
+
+        it('should update the note if it exists', async () => {
+            const newTitle  = 'a' + note.title;
+            note.title = newTitle;
+            await noteRepo.replace(note);
+
+            const matches = await noteRepo.find(new NoteIdSpec(note.id));
+            matches.length.should.eql(1);
+            matches[0].title.should.eql(newTitle);
+        });
+
+        it('should throw NotFoundError if missing', async () => {
+            note.id++;
+
+            let wasThrown = false;
+            try { await noteRepo.replace(note) }
+            catch (err) { wasThrown = true; }
+            wasThrown.should.eql(true);
+        });
+    });
+});
