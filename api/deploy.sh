19d90d167e8cb66734ffe388c11aebdae9d4e68e
Marcus <marcusposey@gmail.com>
2018-03-25 03:54:21 -0500

    Remove old deployment script

--- api/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Deploys the API to the production server.
-
-ssh -o StrictHostKeyChecking=no ${DEPLOY_USER}@${DEPLOY_SRV} << EOF
-  cd ng_core
-  sudo docker-compose pull
-  sudo docker-compose up -d --build
-EOF
